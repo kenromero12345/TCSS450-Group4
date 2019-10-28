fe1d82dedad4dc81a68ee79800979b83bbc92039
Abraham Lee <abelee2016@gmail.com>
2019-10-27 21:39:51 -0700

    Add register validation

--- app/src/main/java/edu/uw/tcss450/tcss450_group4/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/tcss450_group4/MainActivity.java
@@ -3,15 +3,22 @@ package edu.uw.tcss450.tcss450_group4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         // For testing purposes only
-        setContentView(R.layout.fragment_register);
+        if(savedInstanceState == null) {
+            if (findViewById(R.id.frame_main_container) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.frame_main_container, new Register())
+                        .commit();
+            }
+        }
     }
 }
--- app/src/main/java/edu/uw/tcss450/tcss450_group4/Register.java
+++ app/src/main/java/edu/uw/tcss450/tcss450_group4/Register.java
@@ -8,19 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class Register extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
 //    private OnFragmentInteractionListener mListener;
 
     public Register() {
@@ -28,20 +18,11 @@ public class Register extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_register, container, false);
-        ((Button)v.findViewById(R.id.register_register)).setOnClickListener(view -> validRegister(v));
+        v.findViewById(R.id.register_register).setOnClickListener(view -> validRegister(v));
         return v;
     }
 
@@ -115,10 +96,10 @@ public class Register extends Fragment {
             passwordError = true;
         }
 
+        // TODO: Implement transition to verify page
+        // If valid go to Verify page
         if (firstNameError && lastNameError && nicknameError && emailError && passwordError) {
             Log.d("REGISTER", "It's valid");
-//            Credentials toSend = new Credentials.Builder(emailStr, passwordStr).build();
-//            mListener.onRegisterSuccess(toSend);
         }
     }
 
