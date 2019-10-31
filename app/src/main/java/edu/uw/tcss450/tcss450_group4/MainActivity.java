ddc26338d244e542ec1516f90f2b63f219d81d09
Abraham Lee <abelee2016@gmail.com>
2019-10-31 13:50:23 -0700

    Merge branch 'cle1992' into abe

--- app/src/main/java/edu/uw/tcss450/tcss450_group4/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/tcss450_group4/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.uw.tcss450.tcss450_group4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import edu.uw.tcss450.tcss450_group4.ui.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment f = getSupportFragmentManager().findFragmentById(R.id.nav_home);
+        if (f instanceof HomeFragment) {
+
+            this.finish();
+        } else {
+            super.onBackPressed();
+        }
+
     }
 }
